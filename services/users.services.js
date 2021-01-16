336e80009832a051495eb123273b1c24de5171d2
Aleksandr Sventorzhycky <a.o.absolute.zero@gmail.com>
2021-01-16 13:02:39 +0300

    add new index without binding to id, fix function deleteUser

--- services/users.services.js
+++ services/users.services.js
@@ -16,7 +16,7 @@ class User {
 
 class JSONUsersServices {
     addUser = (name) => {
-        let user = new User(users.length + 1, name);
+        let user = new User(new Date().getTime(), name);
         users.push(user);
         this.writeFileJSON();
         return JSON.stringify(users);
@@ -27,14 +27,9 @@ class JSONUsersServices {
     }
 
     deleteUser = id => {
-        if (this.searchUserIndex(id) !== -1) {
-            users.splice(this.searchUserIndex(id), 1);
-            users.forEach((item, index) => {
-                if (this.searchUserIndex(id) < index) {
-                    item.id = index + 1;
-                }
-            })
-
+        let userIndex = this.searchUserIndex(id);
+        if (userIndex !== -1) {
+            users.splice(userIndex, 1);
             this.writeFileJSON();
         }
 
